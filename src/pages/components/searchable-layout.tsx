1534698934153d515bf9e9cb039e2b060642480f
Zero <jgm262@naver.com>
2025-05-29 17:40:56 +0900

    서치바 컴포넌트 제작

--- src/pages/components/searchable-layout.tsx
+++ src/pages/components/searchable-layout.tsx
@@ -1,13 +1,48 @@
-import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+import style from "./searchable-layout.module.css";
 
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  // React.ChangeEvent<HTMLInputElement>는 React에서 발생한 HTML의 input 요소에서 발생한 이벤트를 뜻함.
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
-      <div>임시 서치바</div>
+      <div className={style.searchbar_container}>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요 ..."
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
       {children}
     </div>
   );
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import SearchableLayout from "../components/searchable-layout";
 
 export default function Search() {
   const router = useRouter();
@@ -7,3 +9,7 @@ export default function Search() {
 
   return <h1> Search {q}</h1>;
 }
+
+Search.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
